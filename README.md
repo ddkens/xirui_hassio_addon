2b277cf4f7c893320a30e693b0ecdc7be8f0c01d
Sunbelife <Sunbelife@foxmail.com>
2019-05-22 19:56:53 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # xirui_hassio_addon
-xirui addon for hassio
+
+> xirui addon for hassio
+
+一个西睿空气质量检测仪的 Hassio 插件。
+A Addon for Siemens Xirui Air Quality Detector.
+
+已经在 v0.9.2 版本测试通过。
+tested on v0.9.2
+
+作者：@Sunbelife
+Made by @Sunbelife
---
+++ __init__.py
@@ -0,0 +1,11 @@
+DOMAIN = 'xirui'
+
+ATTR_NAME = 'xirui'
+DEFAULT_NAME = 'xirui'
+
+
+def setup(hass, config):
+	"""Set up is called when Home Assistant is loading our component."""
+
+	# Return boolean to indicate that initialization was successfully.
+	return True
