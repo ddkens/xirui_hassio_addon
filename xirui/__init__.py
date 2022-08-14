b580b89ce768d9a5322da232b8a7878d55b2a4df
ddkens <43508160+ddkens@users.noreply.github.com>
2022-08-14 22:47:06 +0800

    Update __init__.py

--- xirui/__init__.py
+++ xirui/__init__.py
@@ -1,11 +1,1 @@
-DOMAIN = 'xirui'
 
-ATTR_NAME = 'xirui'
-DEFAULT_NAME = 'xirui'
-
-
-def setup(hass, config):
-	"""Set up is called when Home Assistant is loading our component."""
-
-	# Return boolean to indicate that initialization was successfully.
-	return True
